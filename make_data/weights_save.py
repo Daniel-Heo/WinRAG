48f92a24c827654ab0446ac8395afd4b80ac0724
Daniel-Heo <160463127+Daniel-Heo@users.noreply.github.com>
2025-02-16 19:19:58 +0900

    Update weights_save.py

--- make_data/weights_save.py
+++ make_data/weights_save.py
@@ -10,7 +10,7 @@ model = AutoModel.from_pretrained(MODEL_NAME, output_hidden_states=True)
 # 임베딩 레이어 가중치 추출 및 저장
 embedding_weights = model.get_input_embeddings().weight.detach().cpu().numpy()
 
-# 8192 차원을 4개씩 평균내어 2048 차원으로 축소 : 1.5G(5120) -> 778M(2560) -> 389M(1280) -> 194M(640) -> 97M(320) -> 48M(160)
+# 5120 차원을 2개씩 평균내어 2560 차원으로 축소 : 1.5G(5120) -> 778M(2560) -> 389M(1280) -> 194M(640) -> 97M(320) -> 48M(160)
 # (152064, 5120) -> (152064, 2560)
 compressed_weights = embedding_weights.reshape(152064, 2560, 2).mean(axis=2)
 
