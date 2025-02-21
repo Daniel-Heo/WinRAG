a38a5c82334f64d307fe09ddc98081032ad1ae05
Daniel-Heo <160463127+Daniel-Heo@users.noreply.github.com>
2025-02-21 21:21:25 +0900

    Update test_main.cpp

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -6,6 +6,9 @@ int test_mapping() {
     // tokenizer 파일 로드
     loadTokenizer("tokenizer.json");
 
+	// 가중치 파일 로드
+    WeightLoader loader("embedding_weights.npy");
+
     // 테스트할 문장 입력
    // std::string text = "Deep learning improves AI models";
     std::string text = "딥러닝은 AI 모델을 개선합니다.";
@@ -14,8 +17,21 @@ int test_mapping() {
     // 문장 토큰화
     tokens = tokenize(text);
 
-    // 가중치 가져오기
-
+	// 토큰으로 가중치 가져오기 
+	std::vector<std::vector<float>> weights;
+	int lenCount;
+	for (const auto& token : tokens) {
+		std::cout << token.key << " ";
+		weights.push_back(loader.get(token.id));
+		std::cout << "Weight Data[";
+		lenCount = 0;
+		for (const auto& w : loader.get(token.id)) {
+			std::cout << w << " ";
+			lenCount++;
+			if (lenCount > 10) break;
+		}
+		std::cout << "]" << std::endl;
+    }
 
     return 0;
 }
@@ -24,8 +40,8 @@ int main() {
     //SetConsoleOutputCP(CP_UTF8);
     
 	//test_sentencepiece(); // 1.45초 // 0.033
-	test_weights();
-	//test_mapping();
+	//test_weights();
+	test_mapping();
     //test_math();
     return 0;
 }
